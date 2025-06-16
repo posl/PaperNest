97825420dcd6da61a73235eb92e717e5ff009203
y0u1k2i3 <0gl78qs1ciwork@gmail.com>
2025-06-16 13:09:56 +0900

    論文への質問APIを作成 & 英語翻訳システムを修正 & READMEを少し変更

--- README.md
+++ README.md
@@ -27,8 +27,8 @@ docker compose up
 
 ### 3. Webにアクセス
 
-backend -> http://localhost:8000/  
-frontend -> http://localhost:3000/  
+backend -> http://localhost:8000/
+frontend -> http://localhost:3000/
 
 
 
@@ -68,41 +68,16 @@ uvicorn backend.main:app --reload
 http://127.0.0.1:8000/docs
 ```
 
-##
-以下はCLIによるAPI動作確認です．基本的には，SwaggerAPIによる動作確認を推奨します．
 
-## 論文の登録
-- サーバーを起動したターミナルとは別のターミナルで以下を実行してください．
-```bash
-curl -X POST "http://127.0.0.1:8000/upload" -F "file=@/absolute/path/to/your/pdf" -F "category=your_research_category"
-```
-- 論文がデータベースに登録されます．
-- 登録ができたかどうかのレスポンスが返ってきます．
+---
+<br>
+<details>
+<summary><strong>開発者向け情報</strong></summary>
 
-## 論文の全件取得
-- サーバーを起動したターミナルとは別のターミナルで以下を実行してください．
-```bash
-curl http://127.0.0.1:8000/get_all_papers
-```
-- 全ての論文の全てのメタデータが返ってきます．
+## プロジェクト概要
 
-## 質問と類似した論文の検索
-- サーバーを起動したターミナルとは別のターミナルで以下を実行してください．(以下は英語での質問)
-```bash
-curl -X POST "http://127.0.0.1:8000/search" -H "Content-Type: application/json" -d '{"question": "(Please input your query)", "lang": "en"}'
-```
-- 質問と類似した論文のPAPER_ID，URLが返ってきます．
+## 技術スタック
+- フロントエンド: React
+- バックエンド: FastAPI
 
-## 論文の情報更新
-- サーバーを起動したターミナルとは別のターミナルで以下を実行してください．
-```bash
-curl -X PUT "http://127.0.0.1:8000/papers/update" -H "Content-Type: application/json" -d '{"paper_id": "your_paper_id", "field": "field_you_want_to_update", "value": destination_value}'
-```
-- 情報の更新が行われ，{"message":"データの更新が完了しました．"}が返ってきます．
-
-## 論文の削除
-- サーバーを起動したターミナルとは別のターミナルで以下を実行してください．
-```bash
-curl -X DELETE "http://127.0.0.1:8000/papers/delete/your_paper_id"
-```
-- 論文の削除が行われ，{"message":"論文の削除が完了しました。"}が返ってきます．
+## 起動方法
--- backend/api/vector_search/vector_search.py
+++ backend/api/vector_search/vector_search.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from dotenv import load_dotenv
 from fastapi import APIRouter, Depends, HTTPException
@@ -36,18 +37,41 @@ else:
     )
 
 
-def ask_llm(
+# 英数字記号のみの文字列かどうかをチェック
+def is_alnum_symbol(s):
+    return re.fullmatch(r"[!-~]+", s) is not None
+
+
+# クエリの翻訳
+def translate_query(question: str) -> str:
+    client = Groq(api_key=groq_api_key)
+    system_prompt = "You are an excellent translator."
+    user_prompt = f"Please translate the following text into English. However, please include only thetranslation in the output. Also, if the following text is already written in English, pleaseoutput it as is.\n\n{question}"
+    chat_completion = client.chat.completions.create(
+        model=CHAT_MODEL,
+        messages=[
+            {"role": "system", "content": system_prompt},
+            {"role": "user", "content": user_prompt},
+        ],
+    )
+    print(f"Original question: {question}")
+    new_question = chat_completion.choices[0].message.content
+    print(f"Translated question: {new_question}")
+    return new_question
+
+
+# LLMに質問を投げ，回答を生成し，ベクトル検索を行う
+def get_similary_papers(
     query: str, vectorstore: FAISS, llm: ChatGroq, k: int, user_id: int, category: str
 ):
     vectorstoreindex = VectorStoreIndexWrapper(vectorstore=vectorstore)
     # LLMに質問を投げ，回答を生成
     answer = vectorstoreindex.query(query, llm)
-    # print(f"Answer: {answer}")
+    print(f"Answer: {answer}")
 
     results = []
     paper_ids = set()
     db = SessionLocal()
-    # debug = []
     # LLMの回答を分割し，分割されたそれぞれの文に対してベクトル検索
     for sentence in answer.split("\n"):
         if len(results) >= k:
@@ -58,40 +82,46 @@ def ask_llm(
             "category": category,
         }
         for res, score in vectorstore.similarity_search_with_score(
-            sentence, k=k, filter=filter
+            query=sentence, k=k, filter=filter
         ):
             if len(results) >= k:
                 break
-            paper_id = res.metadata["source"]
+            paper_id = res.metadata["paper_id"]
             # paper_idが重複しないようにする
             if paper_id in paper_ids:
                 continue
             paper_ids.add(paper_id)
-            paper = db.query(Paper).filter(Paper.paper_id == paper_id).first()
-            # 論文が存在する場合は，情報を返す
-            if paper:
-                results.append(
-                    VectorSearchResponseSchema(
-                        paper_id=paper.paper_id,
-                        title=paper.title,
-                        authors=paper.authors,
-                        year=paper.year,
-                        conference=paper.conference,
-                        bibtex=paper.bibtex,
-                        citations=paper.citations,
-                        core_rank=paper.core_rank,
-                        pdf_url=paper.pdf_url,
-                        category=paper.category,
-                        summary=paper.summary,
-                        llm_answer=answer,
-                        similarity=score,
-                        chunk_text=res.page_content,
+            try:
+                paper = db.query(Paper).filter(Paper.paper_id == paper_id).first()
+                # 論文が存在する場合は，情報を返す
+                if paper:
+                    results.append(
+                        VectorSearchResponseSchema(
+                            paper_id=paper.paper_id,
+                            title=paper.title,
+                            authors=paper.authors,
+                            year=paper.year,
+                            conference=paper.conference,
+                            bibtex=paper.bibtex,
+                            citations=paper.citations,
+                            core_rank=paper.core_rank,
+                            pdf_url=paper.pdf_url,
+                            category=paper.category,
+                            summary=paper.summary,
+                            llm_answer=answer,
+                            similarity=score,
+                            chunk_text=res.page_content,
+                        )
                     )
+                    print(f"Debug info: {paper.paper_id}, {paper.title}, {paper.pdf_url}")
+                else:
+                    raise HTTPException(
+                        status_code=404, detail=f"論文{paper_id}が見つかりませんでした．"
+                    )
+            except Exception as e:
+                raise HTTPException(
+                    status_code=500, detail=f"データベースからの取得中にエラーが発生しました: {e}"
                 )
-                # debug.append({"paper_id": paper.paper_id, "title": paper.title, "url": paper.pdf_url})
-                print(f"Debug info: {paper.paper_id}, {paper.title}, {paper.pdf_url}")
-            else:
-                raise HTTPException(status_code=404, detail="Paper not found")
 
     db.close()
     # print(f"Debug info: {debug}")
@@ -101,30 +131,19 @@ def ask_llm(
 # PDF検索質問を受け取り，類似したPDFを返す
 @router.post("/search", response_model=list[VectorSearchResponseSchema])
 async def vector_search(
-    query: VectorSearchRequestSchema, current_user: User = Depends(get_current_user)
+    query: VectorSearchRequestSchema,
+    current_user: User = Depends(get_current_user),
 ):
     user_id = current_user.id
     question = query.question
-    language = query.language
     category = query.category
 
     # 質問が英語以外の言語の場合，質問を英語翻訳して回答させる
-    if language != "en":
-        client = Groq(api_key=groq_api_key)
-        system_prompt = "You are an excellent translator."
-        user_prompt = f"Please translate the following text into English. However, please include only the translation results in your output.\n{question}"
-        chat_completion = client.chat.completions.create(
-            model=CHAT_MODEL,
-            messages=[
-                {"role": "system", "content": system_prompt},
-                {"role": "user", "content": user_prompt},
-            ],
-        )
-        print(f"Original question: {question}")
-        question = chat_completion.choices[0].message.content
-        print(f"Translated question: {question}")
+    if not is_alnum_symbol(question):
+        question = translate_query(question)
 
-    return ask_llm(
+
+    return get_similary_papers(
         query=question,
         vectorstore=vector_store,
         llm=llm,
